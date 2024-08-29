41d367e12e74230fe82e23fc7905012f300cc7a4
k-avtonomov <k-avtonomov@yandex.ru>
2024-08-29 09:18:30 +0300

    alter polygon move polygon

--- src/floorplan.js
+++ src/floorplan.js
@@ -31,13 +31,6 @@ export default function floorplan() {
 
     var onSelectZone = () => null;
     var onSelectSensor = () => null;
-    // panZoomEnabled = true,
-    // maxZoom = 5,
-    // toolsWidth = 95,
-    // xScale = d3.scaleLinear(),
-    // yScale = d3.scaleLinear(),
-    // svgCanvas = null,
-    // g = null;
 
     function map() {
         return this
@@ -79,27 +72,29 @@ export default function floorplan() {
         }
     }
 
-    function alterPolygon(point, polyPoints) {
+    function alterPolygon({ sender, point, dx, dy}) {
+        var circles = sender.selectAll('circle');
+        var polygon = sender.select('polygon');
+        
+        if (point) {
+            point.attr("cx", dx).attr("cy", dy);
+        } 
+        
+        var points = circles.nodes();
         var alteredPoints = [];
-        var parentNode = d3.select(this.parentNode);
-
-        //select only the elements belonging to the parent <g> of the selected circle
-        var circles = d3.select(this.parentNode).selectAll('circle');
-        var polygon = d3.select(this.parentNode).select('polygon');
-
-
-        var pointCX = d3.event.x;
-        var pointCY = d3.event.y;
-
-        point.attr("cx", pointCX).attr("cy", pointCY);
-
-        for (var i = 0; i < polyPoints.length; i++) {
-
-            var circleCoord = d3.select(circles._groups[0][i]);
-            var pointCoord = [parseInt(circleCoord.attr("cx")), parseInt(circleCoord.attr("cy"))];
-            alteredPoints[i] = pointCoord;
-
-        }
+        for (var i = 0; i < points.length; i++) {
+            const circle = points[i];
+            const cx = Number(circle.getAttribute('cx'));
+            const cy = Number(circle.getAttribute('cy'));
+            
+            if (!point) {
+                circle.setAttribute("cx", cx + dx);
+                circle.setAttribute("cy", cy + dy);
+                alteredPoints.push([cx + dx, cy + dy]);
+            } else {
+                alteredPoints.push([cx, cy]);
+            }
+        }    
 
         polygon.attr("points", alteredPoints);
 
@@ -126,32 +121,6 @@ export default function floorplan() {
         };
     }
 
-
-    function movePolygon(polyPoints) {
-        var alteredPoints = [];
-
-        //select only the elements belonging to the parent <g> of the selected circle
-        var circles = d3.select(this).selectAll('circle');
-        var polygon = d3.select(this).select('polygon');
-
-
-        var pointdX = d3.event.x;
-        var pointdY = d3.event.y;
-
-        for (var i = 0; i < polyPoints.length; i++) {
-
-            var circleCoord = d3.select(circles._groups[0][i]);
-            var pointCoord = [parseInt(circleCoord.attr("cx")) + pointdX, parseInt(circleCoord.attr("cy")) + pointdY];
-            circleCoord.attr("cx", pointCoord[0]).attr("cy", pointCoord[1]);
-            alteredPoints[i] = pointCoord;
-
-        }
-
-        polygon.attr("points", alteredPoints);
-
-        return alteredPoints;
-    }
-
     //Called on mousedown if mousedown point if a polygon handle
     function drawPolygon(svgCanvas, zone, gPoly) {
         var polyPoints = zone.points;
@@ -165,7 +134,12 @@ export default function floorplan() {
         var dragBehavior = d3.drag()
             .on("drag", function () {
                 if (selectedZone === zone) {
-                    const alteredPoints = alterPolygon.bind(this)(d3.select(this), zone.points)
+                    const alteredPoints = alterPolygon({
+                        sender: d3.select(this.parentNode),
+                        point: d3.select(this), 
+                        dx: d3.event.x, 
+                        dy: d3.event.y
+                    })
                     zone.points = alteredPoints;
                     selectedZone = zone;
                     d3.select(".polygon-id-" + zone.id + '-text').remove();
@@ -177,9 +151,6 @@ export default function floorplan() {
                 selectPolygon(selectedZone)
             });
 
-        // Not needed while drawing them all at first.
-        // polyPoints.splice(polyPoints.length - 1);
-        //console.log(polyPoints);
 
         var polyEl = gPoly.append("polygon")
             .attr("points", polyPoints);
@@ -224,7 +195,13 @@ export default function floorplan() {
             .on("end", function (d) {
                 if (selectedZone === zone) {
                     d3.select(this).attr("transform", `translate(${d.x = 0},${d.y = 0})`)
-                    const alteredPoints = movePolygon.bind(this)(zone.points);
+
+                    const alteredPoints = alterPolygon({
+                        sender: d3.select(this),
+                        dx: d3.event.x, 
+                        dy: d3.event.y
+                    });
+
                     zone.points = alteredPoints;
                     selectedZone = zone;
                     d3.select(".polygon-id-" + zone.id + '-text').remove();
@@ -234,11 +211,8 @@ export default function floorplan() {
                 }
             }));
 
-        // Add context menu
-        // gPoly.on('contextmenu', contextMenu(menu));
         gPoly.on('click', () => selectPolygon(zone));
 
-        // Add label text
         var gPolyCentroid = d3.polygonCentroid(polyPoints);
 
         addLabel(zone.name, gPolyCentroid, "polygon-id-" + zone.id + '-text', gPoly);
@@ -338,9 +312,6 @@ export default function floorplan() {
         zones.forEach(function (zone) {
             var gPoly;
             drawPolygon(svgCanvas, zone, gPoly);
-
-
-
         });
     };
 
@@ -356,11 +327,12 @@ export default function floorplan() {
         var linePoint1, linePoint2;
         var polyPoints = zone.points;
 
-        var polyDraw = svgCanvas.on("mousedown", setPoints)
+        // var polyDraw = 
+        svgCanvas.on("mousedown", setPoints)
             .on("mousemove", drawline)
             .on("mouseup", decidePoly);
 
-        var dragBehavior = d3.drag()
+        // var dragBehavior = d3.drag()
         // .on("start", () => console.log("start"))
         // .on("drag", alterPolygon)
         // .on("end", () => console.log("end"));
